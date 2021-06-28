054e5a92a8f64fc1fae51648662904072176cea0
Hadeer Fawzy <hadeerfawzymahmoud92@gmail.com>
2021-06-28 21:41:02 +0200

    install eslint, create folders structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { lightTheme, darkTheme } from "config/theme";
 
-function App() {
+const App = () => {
+  const [themeToggle, ] = useState("light");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider
+      theme={
+        themeToggle === "dark"
+          ? createMuiTheme(darkTheme)
+          : createMuiTheme(lightTheme)
+      }
+    >
+      test test test 
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
